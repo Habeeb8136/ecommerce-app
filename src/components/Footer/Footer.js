130eb89a5fe1ed899f85f7baf412b39f61422741
Habeeb8136 <habeeb041099@gmail.com>
2022-12-13 18:30:00 +0530

    changed href

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
         <footer>
             <ul className="footer-nav">
                 <li><a href="/">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Terms</a></li>
-                <li><a href="">Privacy</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Terms</a></li>
+                <li><a href="/">Privacy</a></li>
             </ul>
             <p className="copyright">copyright © {currentYear}</p>
         </footer>
