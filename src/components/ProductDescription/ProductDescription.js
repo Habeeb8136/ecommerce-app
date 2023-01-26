8e77d9807c864b48b855669ae13a837b41186d49
Habeeb8136 <habeeb041099@gmail.com>
2023-01-26 21:38:01 +0530

    Added dispatch action

--- src/components/ProductDescription/ProductDescription.js
+++ src/components/ProductDescription/ProductDescription.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import {useParams} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import './ProductDescription.css'
+import { addtoCart } from '../../features/cart';
 
 
 const ProductDescription = () => {
 
+const dispatch=useDispatch()
 const[product,setProduct]=useState([]);
 
     const params=useParams()
@@ -28,6 +31,11 @@ useEffect(() => {
       });
   }, [id]);
 
+  const handleAddtoCart=(product)=>{
+    dispatch(addtoCart(product))
+    console.log('added',product)
+  }
+
     return (
         <Container md={4} l={4} className='product-description'>
             <div className='image-container'>
@@ -44,7 +52,7 @@ useEffect(() => {
 
                 
                 <div className='price-des'><h2>$ {product.price}</h2></div>
-                <Button variant='info' className='checkout-btn'>Add to Cart</Button>
+                <Button variant='info' className='checkout-btn' onClick={()=>handleAddtoCart(product)}>Add to Cart</Button>
                 
                 
                 
