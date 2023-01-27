9cb5a8d43bc4705bd56c075694f91106e4069a46
Habeeb8136 <habeeb041099@gmail.com>
2023-01-27 20:57:28 +0530

    shopping cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,42 @@
 import React from 'react';
-
+import './Cart.css'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-    
+    const cartItems=useSelector(state=>state.cart)
+    console.log(cartItems)
     return (
         
         <div className='cart-page'>
+        <h1>Shopping Cart</h1>
+        <hr></hr>
+        <br></br>
         <div className='cart-container'>
-            <h2>NO ITEMS IN CART</h2>
+        {cartItems.items.length===0?
+        <>
+            <h2>Your shopping card is empty</h2>
+            <Link to='/'>
+                <p>browse products</p>
+            </Link>
+        </>
+        :
+        cartItems.items.map((items)=>{
+            return (
+                <div key={items.id}>
+                <p>
+                    {items.title}
+                </p>
+                <p>
+                    {items.price}
+                </p>
+                <p>
+                    {items.itemQuantity}
+                </p>
+                </div>
+            )
+        })
+        }
             
         </div>
         </div>
